00465f66677a9676c1634de94dd88e65183cfbd6
kevinmaiden7 <kevin.m.gallego@gmail.com>
2019-09-25 15:15:50 -0500

    CORS - HEADER

--- MRI/app.js
+++ MRI/app.js
@@ -19,8 +19,8 @@ let db = admin.firestore(); // Acesso a Firestore
 app.use(express.json());
 
 app.use((req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","");
-  res.header("Access-Control-Allow-Headers","");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   if(req.method ==='OPTIONS'){
   res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
   return res.status(200).json({});
