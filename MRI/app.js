a5e8ad5b5489a6b974aa7b0de18508ec700d2b7c
Daniel Vanegas <danieru1313@gmail.com>
2019-09-25 01:27:34 -0500

    Add CORS middleware

--- MRI/app.js
+++ MRI/app.js
@@ -15,7 +15,18 @@ admin.initializeApp({
 });
 let db = admin.firestore(); // Acesso a Firestore
 
+// MIDDLEWARES
 app.use(express.json());
+
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","");
+  res.header("Access-Control-Allow-Headers","");
+  if(req.method ==='OPTIONS'){
+  res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+  return res.status(200).json({});
+  }
+  next();
+  });
 
 //GET
 app.get('/incidentes', async function (req, res) {
